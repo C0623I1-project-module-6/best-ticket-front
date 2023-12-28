468b268f0dd8f94cb5c6ef68ccba1b68664d7353
AN-HA\AN HA <baoandlk93@gmail.com>
2023-12-28 14:32:52 +0700

    save version of code demo v-1

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,179 +1,133 @@
-import {Link, useNavigate} from "react-router-dom";
-
+import {useNavigate} from "react-router-dom";
+import logo from "../assets/img/logo/best-ticket-bg-v3.png"
+import background from "../assets/img/logo/best-ticket-bg-v4.png"
+import {FaFacebook, FaGithub, FaGoogle} from "react-icons/fa6";
 
 function Register() {
     const navigate = useNavigate();
 
     return (
-        <div className="flex-col items-center justify-center text-center">
-            <button
-                type="button"
-                className="btn btn-circle mt-2 btn-outline btn-primary"
-                onClick={() => navigate("/home")}
-            >
-                Home
-            </button>
-            <div className="bg-white px-6 py-24 sm:py-32 lg:px-8 ">
-                <div
-                    className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-                    aria-hidden="true"
-                >
-                    <div
-                        className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
+        <div className="flex items-center justify-center text-center gap-3 bg-white">
+
+            <div className="flex bg-white rounded-lg items-center  flex-1 flex-col justify-center lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm m-0">
+                    <img
+                        className="mx-auto h-[150px] w-[200px] cursor-pointer"
+                        src={logo}
+                        alt="Your Company"
+                        onClick={() => navigate("/")}
                     />
-                </div>
-                <div className="mx-auto max-w-2xl text-center items-end justify-items-end flex-col ">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Registration</h2>
-                    <p className="mt-2 text-lg leading-8 text-gray-600">
-                        Complete to member of we
-                    </p>
+                    <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
                 </div>
 
-                <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20 ">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-3" action="#" method="POST">
                         <div>
-                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                                First name
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="text"
-                                    name="first-name"
-                                    id="first-name"
-                                    autoComplete="given-name"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="email"
+                                       className="block text-sm font-medium leading-6 text-gray-900
+                                       hover:text-gray-500 cursor-pointer">
+                                    Username
+                                </label>
                             </div>
-                        </div>
-                        <div>
-                            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Last name
-                            </label>
-                            <div className="mt-2.5">
+                            <div className="mt-2">
                                 <input
-                                    type="text"
-                                    name="last-name"
-                                    id="last-name"
-                                    autoComplete="family-name"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Username
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="text"
-                                    name="company"
-                                    id="username"
-                                    autoComplete="organization"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Password
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="password"
-                                    name="company"
-                                    id="password"
-                                    autoComplete="organization"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="confirmPassword"
-                                   className="block text-sm font-semibold leading-6 text-gray-900">
-                                Confirm Password
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="password"
-                                    name="company"
-                                    id="confirmPassword"
-                                    autoComplete="organization"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-                                Email
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="email"
-                                    name="email"
                                     id="email"
+                                    name="email"
+                                    required
+                                    type="text"
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="phone-number"
-                                   className="block text-sm font-semibold leading-6 text-gray-900">
-                                Phone number
-                            </label>
-                            <div className="relative mt-2.5">
-                                <div className="absolute inset-y-0 left-0 flex items-center">
-                                    <label htmlFor="country" className="sr-only">
-                                        Country
-                                    </label>
-                                    <select
-                                        id="country"
-                                        name="country"
-                                        className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                                    >
-                                        <option>US</option>
-                                        <option>VI</option>
-                                        <option>EU</option>
-                                    </select>
 
-                                </div>
+                        <div>
+                            <div className="flex items-center justify-between m-0">
+                                <label htmlFor="password"
+                                       className="block text-sm font-medium leading-6 text-gray-900
+                                       hover:text-gray-500 cursor-pointer">
+                                    Password
+                                </label>
+                            </div>
+                            <div className="">
                                 <input
-                                    type="tel"
-                                    name="phone-number"
-                                    id="phone-number"
-                                    autoComplete="tel"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1
+                                    ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                                    focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
+                        <div className="flex justify-center gap-3">
+                            <div className="w-full ">
+                                <div className="w-full btn btn-outline btn-primary">
+                                    Sign in
+                                </div>
+                            </div>
+                        </div>
 
+                    </form>
+                    <div className="w-full mt-3 flex justify-between text-sm">
+                        <span
+                            className="cursor-pointer text-blue-500 hover:text-gray-500 font-bold">Forgot password ?</span>
+                        <div className="flex">
+                                <span className="mx-2 font-bold">
+                                    Don't have account ?
+                                </span>
+                            <span className="cursor-pointer hover:text-gray-500 font-bold text-blue-500"
+                                  onClick={() => navigate("/login")}>Register</span>
+                        </div>
                     </div>
-                    <div className="mt-10 items-center justify-center text-center gap-1">
-                        <button
-                            type="submit"
-                            className="btn btn-outline btn-primary w-full"
-                        >
-                            Register
-                        </button>
-                        <p className="m-1">Have account ?</p>
-                        <button
-                            type="button"
-                            className="btn btn-outline btn-accent w-full"
-                            onClick={() => navigate("/login")}
-                        >
-                            Login
-                        </button>
+                    <div className="mt-3">
+                        OR
                     </div>
-                </form>
-
+                    <div className="flex justify-center gap-3 mt-2 h-[32px]">
+                        <div className="w-full">
+                            <div
+                                className="w-full h-full flex gap-3 items-center border cursor-pointer text-dark-purple
+                                hover:bg-[#ecf5ff] hover:text-[#409eff]
+                                justify-center rounded-full "
+                            >
+                                <FaFacebook className="text-[#3b5998]"/>
+                                Facebook
+                            </div>
+                        </div>
+                        <div className="w-full ">
+                            <div
+                                className="w-full h-full flex gap-3 items-center border cursor-pointer text-dark-purple
+                                hover:bg-[#ecf5ff] hover:text-[#409eff]
+                                justify-center rounded-full
+                                "
+                            >
+                                <FaGoogle className="text-[#e94820]"/>
+                                Google
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <div
+                                className="w-full h-full flex gap-3 items-center border cursor-pointer text-dark-purple
+                                hover:bg-[#ecf5ff] hover:text-[#409eff]
+                                justify-center rounded-full "
+                            >
+                                <FaGithub/>
+                                Github
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex bg-white rounded-lg items-center  h-screen flex-1 flex-col justify-center lg:px-8">
+                <img src={background} className="w-screen h-screen" alt=""/>
             </div>
         </div>
-    )
-
+    );
 }
 
 export default Register;
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,21 +1,27 @@
-import { BsTicket } from "react-icons/bs";
+import {BsTicket} from "react-icons/bs";
 import SearchBar from "./partials/SearchBar.jsx";
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/img/logo/best-ticket-logo-v3.svg";
 
 const Header = () => {
     return (
-        <div className="px-6 bg-white flex text-gray-800 justify-between items-center text-sm border-b-2 border-gray-400">
-            <div className="flex items-center gap-7 ">
-                <img src={logo} alt="" className="h-[44px] w-[44px]" />
-                <SearchBar />
-            </div>
-            <div className="flex items-center gap-3 font-semibold">
-                <span className="cursor-pointer border-green-600 border-[1px] rounded-3xl text-green-600 font-bold px-6 py-2"> Tạo sự kiện</span>
-                <div className="cursor-pointer flex items-center gap-3">
-                    <BsTicket size={30} />
-                    <span>Đăng nhập|Đăng ký</span>
+        <div className="h-[100px] bg-[#2dc275] text-white">
+            <div className="px-6  flex  justify-center gap-20 items-center text-sm border-b-2 border-gray-400">
+                <div className="flex items-start gap-20 ">
+                    <img src={logo} alt="" className="h-[96px] w-[100px] m-0"/>
+                    <SearchBar/>
                 </div>
+                <div className="flex items-center gap-3 font-semibold">
+                <span className="cursor-pointer border-green-600 border-[1px]
+                hover:bg-light-white hover:text-black
+                rounded-3xl font-bold px-6 py-2">
+                    Tạo sự kiện
+                </span>
+                    <div className="cursor-pointer flex items-center gap-3">
+                        <BsTicket size={30}/>
+                        <span>Đăng nhập|Đăng ký</span>
+                    </div>
 
+                </div>
             </div>
         </div>
     );
