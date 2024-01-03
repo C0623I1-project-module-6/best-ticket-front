8991b4fecfd678174dccc8b7fa1d7e76e30026fa
PhaiP <pphp1998@gmail.com>
2024-01-03 09:02:40 +0700

    search Event Component

--- src/component/content/Content.jsx
+++ src/component/content/Content.jsx
@@ -2,16 +2,22 @@ import LeftSVG from '../../assets/img/heading-img-1-left.svg';
 import RightSVG from '../../assets/img/heading-img-1-right.svg';
 import Event from "./partials/Event.jsx";
 
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css/bundle';
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import { getAllEvent } from "../../redux/features/EventSlice.js";
 
 
 export default function Content(){
+    const dispatch = useDispatch();
+    const events = useSelector((state) => state.event.events);
+    useEffect(() => {
+        dispatch(getAllEvent());
+    }, []);
     return(
        <div className=" w-[70%] bg-white mx-auto mr-[5%] ml-[5%] mt-2 mb-2 rounded-lg ">
            <div className=" w-full">
@@ -59,22 +65,18 @@ export default function Content(){
            <div className="flex justify-center mb-0 space-x-2 ">
                <div className="flex place-items-end justify-center w-[70%] border-b-4 border-gray-800 ">
                    <img src={LeftSVG} alt="" className="" />
-                   <span className="pb-2  border-gray-800 text-green-600 font-bold text-3xl">Sự kiện nổi bật</span>
+                   <span className="pb-2  border-gray-800 text-green-600 font-bold text-3xl">Tất cả sự kiện</span>
                    <img src={RightSVG} alt="" className="" />
                </div>
            </div>
            <div className="w-full overflow-hidden overflow-y-auto mt-2 mr-2">
                <div className="">
                    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-4 p-4">
-                       <Event/>
-                       <Event/>
-                       <Event/>
-                       <Event/>
-                       <Event/>
-                       <Event/><Event/><Event/><Event/>
+                       {events.map((event) => (
+                           <Event key={event.id} event={event} />
+                       ))}
                    </div>
                </div>
-
            </div>
            <div className="pt-10 pb-10 text-center">
                <div className="pt-10 pb-10 text-center">
