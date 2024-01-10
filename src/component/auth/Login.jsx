423a73d473933109203c55e97f3b22b1147f3be9
NhanTran262 <tranhoangnhan262@gmail.com>
2024-01-10 14:47:10 +0700

    no message

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -51,7 +51,7 @@ function Login() {
                                 type="text"
                                 autoComplete="email"
                                 className="block w-full rounded-md border-0 py-1.5
-                                text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                                text-gray-900 ring-1 ring-inset placeholder:text-gray-400
                                 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6
                                 dark:bg-gray-400 dark:text-black
                                 "
