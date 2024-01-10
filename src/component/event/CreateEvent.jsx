2210770c7041542cf89fd25f4629765cc7fa9359
PhaiP <pphp1998@gmail.com>
2024-01-10 10:20:34 +0700

    update code

--- src/component/event/CreateEvent.jsx
+++ src/component/event/CreateEvent.jsx
@@ -1,10 +1,12 @@
 import CreateEventSideBar from "./partials/CreateEventSideBar.jsx";
 import {Button, Input} from "@material-tailwind/react";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { ProvincesApi } from "../../api/ProvincesApi.js";
 import { Select, Option } from "@material-tailwind/react";
 import {findAllEventType} from "../../api/EventTypeApi.js";
 import banner from "../../assets/img/cover-event.jpg"
+import {FcCheckmark} from "react-icons/fc";
+import {IoListOutline} from "react-icons/io5";
 export default function CreateEvent(){
     const [email, setEmail] = useState("");
     const onChange = ({ target }) => setEmail(target.value);
@@ -14,6 +16,10 @@ export default function CreateEvent(){
     const [districts, setDistricts] = useState([]);
     const [selectedProvince,setSelectedProvince] = useState("");
     const [selectedDistrict,setSelectedDistrict] = useState("")
+
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [showOptions, setShowOptions] = useState(false);
+
 
     const fetchApiEventTypes = async () => {
         try {
@@ -33,6 +39,18 @@ export default function CreateEvent(){
         }
         fetchProvinceData();
     }, []);
+
+    const toggleOptions = () => {
+        setShowOptions(!showOptions);
+    };
+    const toggleOption = (option) => {
+        const isSelected = selectedOptions.includes(option);
+        const updatedOptions = isSelected
+            ? selectedOptions.filter((selectedOption) => selectedOption !== option)
+            : [...selectedOptions, option];
+
+        setSelectedOptions(updatedOptions);
+    };
     return(
         <div className="flex h-screen  ">
             <CreateEventSideBar/>
@@ -108,6 +126,49 @@ export default function CreateEvent(){
                                 className="mt-1 p-2 border rounded-md w-full"
                                 placeholder="Chi tiết về sự kiện..."
                             />
+                        </div>
+                    </div>
+                    <div className="flex-row items-center gap-1 w-[70%] mx-auto">
+                        <div
+                            className="flex border-2 border-gray-300 rounded-lg gap-2 cursor-pointer p-2"
+                            onClick={toggleOptions}
+                        >
+                            <IoListOutline size={23} />
+                            <div>
+                                {selectedOptions.length === 0
+                                    ? 'Tất cả sự kiện'
+                                    : selectedOptions.join(',')}
+                            </div>
+                        </div>
+                        <div className="relative z-10 w-full">
+                            {showOptions && (
+                                <div className="absolute top-2 right-0 bg-white w-full rounded-lg">
+                                    <div
+                                        onClick={() => setSelectedOptions([])}
+                                        className="cursor-pointer hover:bg-green-200 p-2 rounded-lg"
+                                    >
+                                        Tất cả sự kiện
+                                    </div>
+                                    {eventTypes.map((eventType, index) => (
+                                        <div
+                                            key={index}
+                                            onClick={() => toggleOption(eventType.name)}
+                                            className="cursor-pointer p-2 hover:bg-green-200 rounded-lg border-gray-600"
+                                        >
+                                            <div className="flex items-center gap-2">
+                                                {eventType.name}
+                                                <FcCheckmark
+                                                    className={
+                                                        selectedOptions.includes(eventType.name)
+                                                            ? ''
+                                                            : 'hidden'
+                                                    }
+                                                />
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
