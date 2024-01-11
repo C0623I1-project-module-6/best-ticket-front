a4bf3ae63934e71c66c9bbf00b06aed6a5cfad28
138636784+thinhphan1122@users.noreply.github.com <thinhphanvn8499@gmail.com>
2024-01-11 14:56:30 +0700

    Add TicketPayment.jsx

--- src/component/ticket/TicketCustomerInfo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TicketCustomerInfo (){
-    return(
-        <>
-            <div className="w-full bg-slate-300">hel</div>
-        </>
-    );
-}
