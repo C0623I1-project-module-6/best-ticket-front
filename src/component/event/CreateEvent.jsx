4c42b884df3ef6247094405c4e78026f4f3b7408
PhaiP <pphp1998@gmail.com>
2024-01-15 02:29:59 +0700

    update create event

--- src/component/event/CreateEvent.jsx
+++ src/component/event/CreateEvent.jsx
@@ -7,19 +7,16 @@ import {findAllEventType} from "../../api/EventTypeApi.js";
 import banner from "../../assets/img/cover-event.jpg"
 import {FcCheckmark} from "react-icons/fc";
 import {IoListOutline} from "react-icons/io5";
+import SelectEvent from "./partials/SelectEvent.jsx";
 export default function CreateEvent(){
     const [email, setEmail] = useState("");
     const onChange = ({ target }) => setEmail(target.value);
-    const [selectedEventTypes, setSelectedEventTypes] = useState([]);
     const [eventTypes, setEventTypes] = useState([]);
     const [provinces, setProvinces] = useState([]);
     const [districts, setDistricts] = useState([]);
     const [selectedProvince,setSelectedProvince] = useState("");
-    const [selectedDistrict,setSelectedDistrict] = useState("")
-
-    const [selectedOptions, setSelectedOptions] = useState([]);
-    const [showOptions, setShowOptions] = useState(false);
-
+    const [selectedDistrict,setSelectedDistrict] = useState("");
+    const [selectedEventTypes,setSelectedEventTypes] = useState([])
 
     const fetchApiEventTypes = async () => {
         try {
@@ -39,24 +36,14 @@ export default function CreateEvent(){
         }
         fetchProvinceData();
     }, []);
-
-    const toggleOptions = () => {
-        setShowOptions(!showOptions);
-    };
-    const toggleOption = (option) => {
-        const isSelected = selectedOptions.includes(option);
-        const updatedOptions = isSelected
-            ? selectedOptions.filter((selectedOption) => selectedOption !== option)
-            : [...selectedOptions, option];
-
-        setSelectedOptions(updatedOptions);
-    };
+    const selectedData = (data) =>{
+        setEventTypes(data);
+    }
     return(
         <div className="flex h-screen  ">
             <CreateEventSideBar/>
-            <div className="w-[70vw] overflow-y-auto ">
+            <div className="w-full overflow-y-auto ">
                 <div className=" mx-auto">
-                    {/*<img src={banner} alt="" className="w-full object-cover bg-gradient-to-b from-blue-500" />*/}
                     <div className="relative overflow-hidden">
                         <img src={banner}  className="w-full  object-cover"  alt=""/>
                         <div className="absolute inset-0 bg-gradient-to-b from-blue-500 opacity-50"></div>
@@ -103,17 +90,7 @@ export default function CreateEvent(){
                     <div className="w-[80%] p-5 mx-auto">
                         <Input label="Địa chỉ chi tiết" />
                     </div>
-                    <div className="w-[80%] flex gap-5 p-5 mx-auto">
-                        <Select
-                            label=" Thể loại sự kiện "
-                        >
-                            {eventTypes.map((eventType, index) => (
-                                <Option key={index} value={eventType.name}>
-                                    {eventType.name}
-                                </Option>
-                            ))}
-                        </Select>
-                    </div>
+                    <SelectEvent eventTypes={eventTypes} handleData={selectedData}/>
                     <div className="w-[80%] flex gap-5 mx-auto">
                         <div className="p-4 w-full">
                             <label htmlFor="EventDetail" className="block text-sm font-medium text-gray-700">
@@ -128,48 +105,10 @@ export default function CreateEvent(){
                             />
                         </div>
                     </div>
-                    <div className="flex-row items-center gap-1 w-[70%] mx-auto">
-                        <div
-                            className="flex border-2 border-gray-300 rounded-lg gap-2 cursor-pointer p-2"
-                            onClick={toggleOptions}
-                        >
-                            <IoListOutline size={23} />
-                            <div>
-                                {selectedOptions.length === 0
-                                    ? 'Tất cả sự kiện'
-                                    : selectedOptions.join(',')}
-                            </div>
-                        </div>
-                        <div className="relative z-10 w-full">
-                            {showOptions && (
-                                <div className="absolute top-2 right-0 bg-white w-full rounded-lg">
-                                    <div
-                                        onClick={() => setSelectedOptions([])}
-                                        className="cursor-pointer hover:bg-green-200 p-2 rounded-lg"
-                                    >
-                                        Tất cả sự kiện
-                                    </div>
-                                    {eventTypes.map((eventType, index) => (
-                                        <div
-                                            key={index}
-                                            onClick={() => toggleOption(eventType.name)}
-                                            className="cursor-pointer p-2 hover:bg-green-200 rounded-lg border-gray-600"
-                                        >
-                                            <div className="flex items-center gap-2">
-                                                {eventType.name}
-                                                <FcCheckmark
-                                                    className={
-                                                        selectedOptions.includes(eventType.name)
-                                                            ? ''
-                                                            : 'hidden'
-                                                    }
-                                                />
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
+                    <div className="w-[80%] flex items-center justify-center p-5 mx-auto ">
+                        <button className="border-black rounded-lg bg-green-400 py-2 px-5">
+                            Tạo Event
+                        </button>
                     </div>
                 </div>
             </div>
--- src/component/event/partials/CreateEventSideBar.jsx
+++ src/component/event/partials/CreateEventSideBar.jsx
@@ -1,7 +1,29 @@
+import logo from '../../../assets/img/logo/logo-auth-header-light.svg'
+import {useNavigate} from "react-router-dom";
+import {Avatar, Popover, PopoverContent, PopoverHandler} from "@material-tailwind/react";
+import avatar from "../../../assets/img/User.png";
+import {useSelector} from "react-redux";
+
 export default function CreateEventSideBar(){
+    const user = useSelector((state) => state.user.value);
+    const navigate = useNavigate();
     const steps = ["Thông tin sự kiện","Thời gian & loại vé","Cài đặt","Thông tin thanh toán"]
     return(
-        <div className="w-[30vw] bg-purple-800  text-white text-xl ">
+        <div className="w-[25vw] bg-purple-800  text-white text-xl ">
+            <div className="flex gap-5 items-center p-5  justify-between border-b">
+                <img src={logo} alt="" className="h-[60px] w-[60px] m-0 cursor-pointer border-black rounded-lg bg-green-400 "
+                     onClick={() => navigate("/")}/>
+                <div>
+                    <span>{user ? user.name : "User" }</span>
+                    <Avatar
+                        size="25"
+                        alt={avatar}
+                        src={user ? user.avatar : <img src={avatar} alt="" />}
+                        className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
+                    />
+
+                </div>
+            </div>
             {steps.map((step,index)=>(
                 <div className="flex gap-5 items-center hover:bg-white hover:text-black p-5 ">
                     <span className="rounded-full bg-green-400 h-10 w-10 flex items-center justify-center" >{index+1}</span>
