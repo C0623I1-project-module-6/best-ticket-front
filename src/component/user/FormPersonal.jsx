86cbfbe50f0f5de7553596f0f0d97f64d315f417
NhanTran262 <tranhoangnhan262@gmail.com>
2024-01-15 08:20:02 +0700

    Update form Customer/Organizer

--- src/component/user/FormPersonal.jsx
+++ src/component/user/FormPersonal.jsx
@@ -63,30 +63,6 @@ export default function FormPersonal(){
                     </div>
                 </div>
             </div>
-            <h4 className="font-serif text-3xl p-5">Địa chỉ thường trú</h4>
-            <div className="border border-solid border-black rounded-md py-5 px-5 bg-white">
-                <div className="border border-solid border-black rounded-md py-5 px-5 bg-white">
-                    <div className="grid grid-cols-2 gap-4">
-                        <div>
-                            <label className="block text-xl font-serif text-gray-700">
-                                Số điện thoại *
-                            </label>
-                            <input type="text"
-                                   className=" bg-gray-200 block w-full rounded-md border border-solid border-black
-                                        p-2 mt-2 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600
-                                        sm:text-1xl sm:leading-6"/>
-                        </div>
-                        <div>
-                            <label className="block text-xl font-serif text-gray-700">
-                                Email *
-                            </label>
-                            <input type="text"
-                                   className="bg-gray-200 block w-full rounded-md border border-solid border-black
-                                        p-2 mt-2 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600
-                                        sm:text-1xl sm:leading-6"/>
-                        </div>
-                    </div>
-                </div>
-            </div></>
+        </>
     )
 }
