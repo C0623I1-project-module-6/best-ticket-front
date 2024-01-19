ec6b8561c5bb457856e99926646d21beed9f69f2
138636784+thinhphan1122@users.noreply.github.com <thinhphanvn8499@gmail.com>
2024-01-19 13:31:08 +0700

    Minor changes

--- src/layout/pages/errors/NotFoundPage.jsx
+++ src/layout/pages/errors/NotFoundPage.jsx
@@ -15,7 +15,7 @@ const NotFoundPage = () => {
             <div>
                 <button
                     onClick={goBack}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded m-2"
                 >
                     Go to Previous Page
                 </button>
