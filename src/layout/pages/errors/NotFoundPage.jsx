46441168f4ee804a964f7512b46fcc59f3b7b8bd
138636784+thinhphan1122@users.noreply.github.com <thinhphanvn8499@gmail.com>
2024-01-19 13:26:32 +0700

    Add Go to Previous Page function

--- src/layout/pages/errors/NotFoundPage.jsx
+++ src/layout/pages/errors/NotFoundPage.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
+    const goBack = () => {
+        window.history.back();
+    };
+
     return (
-        // eslint-disable-next-line no-undef
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <div className="text-5xl font-bold mb-4 text-red-500">
                 4<span className="text-gray-500">0</span>4
             </div>
             <p className="text-2xl mb-8 text-gray-700">Sorry for the inconvenience!</p>
-            <p className="text-xl mb-8 text-gray-600">
-                Page is under construction.
-            </p>
-            <Link
-                to="/"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-                Go to Home
-            </Link>
+            <p className="text-xl mb-8 text-gray-600">Page is under construction.</p>
+            <div>
+                <button
+                    onClick={goBack}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+                >
+                    Go to Previous Page
+                </button>
+                <Link
+                    to="/"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+                >
+                    Go to Home
+                </Link>
+            </div>
         </div>
     );
 };
