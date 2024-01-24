b8268140cf127f4fe0f3073a531327cb2f8fefe6
DAT <mfdat2015@gmail.com>
2024-01-24 09:52:29 +0700

    doing ticketBookingStep2

--- src/component/ticket/TicketBookingStep2.jsx
+++ src/component/ticket/TicketBookingStep2.jsx
@@ -11,8 +11,12 @@ export const TicketBookingStep2 = () => {
     const user = useSelector(selectUserLogin);
     const seatTickets = useSelector(state => state.seat)
     const tickets = useSelector(selectShowTicketByTimeId);
+    const countVIP = seatTickets.ticketTypes.filter(seat => seat === 'VIP').length;
+    const countTHUONG = seatTickets.ticketTypes.filter(seat => seat === 'THƯỜNG').length;
+    const countLAU = seatTickets.ticketTypes.filter(seat => seat === 'LẦU').length;
+
     console.log(tickets)
-    console.log(seatTickets.ticketTypes);
+    console.log(seatTickets.ticketTypes.length);
     return (
         <>
             <div className="mx-40 text-black py-5">
@@ -123,33 +127,28 @@ export const TicketBookingStep2 = () => {
                                 <div className="flex justify-between py-2" key={index}>
                                     {ticketType === "VIP" && (
                                         <>
-                                            {tickets !== null ? tickets.data.map((ticket, index) => (
-                                                <>
-                                                    <div className="w-2/3" key={index}>
-                                                        {/*{ticket.ticketType.name === }*/}
-                                                        <p>Vé {ticketType}</p>
-                                                        <p>{/* Dữ liệu của loại vé VIP */}</p>
-                                                        <p>{" " + seatTickets.seats}</p>
-                                                    </div>
-                                                    <div className="w-1/3 text-right">
-                                                        <p>Số lượng</p>
-                                                        <p>Giá</p>
-                                                    </div>
-                                                </>
-                                            )) : <div></div>}
-
+                                            <div className="w-2/3" key={index}>
+                                                <p>Vé {ticketType}</p>
+                                                <p>{/* Dữ liệu của loại vé VIP */}</p>
+                                                <p>{" " + seatTickets.seats[index]}</p>
+                                            </div>
+                                            <div className="w-1/3 text-right">
+                                                <p>{countVIP}</p>
+                                                <p>{seatTickets.price[index]} VNĐ</p>
+                                            </div>
                                         </>
+
                                     )}
                                     {ticketType === "THƯỜNG" && (
                                         <>
                                             <div className="w-2/3">
                                                 <p>Vé {ticketType}</p>
                                                 <p>{/* Dữ liệu của loại vé Standard */}</p>
-                                                <p>{" " + seatTickets.seats}</p>
+                                                <p>{" " + seatTickets.seats[index]}</p>
                                             </div>
                                             <div className="w-1/3 text-right">
-                                                <p>Số lượng</p>
-                                                <p>Giá</p>
+                                                <p>{countTHUONG}</p>
+                                                <p>{seatTickets.price[index]} VNĐ</p>
                                             </div>
                                         </>
                                     )}
@@ -158,11 +157,11 @@ export const TicketBookingStep2 = () => {
                                             <div className="w-2/3">
                                                 <p>Vé {ticketType}</p>
                                                 <p>{/* Dữ liệu của loại vé Standard */}</p>
-                                                <p>{" " + seatTickets.seats}</p>
+                                                <p>{" " + seatTickets.seats[index]}</p>
                                             </div>
                                             <div className="w-1/3 text-right">
-                                                <p>Số lượng</p>
-                                                <p>Giá</p>
+                                                <p>{countLAU}</p>
+                                                <p>{seatTickets.price[index]} VNĐ</p>
                                             </div>
                                         </>
                                     )}
